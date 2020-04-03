f3f0a48a4f8090e95ac530fc9efa9c60671a7b4b
eludire oluwadamilola <eludire@ymail.com>
2020-04-03 12:18:51 +0100

    for FI Service

---
+++ ConsentPortal/dbo.NDPR.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [CustomerId] NVARCHAR(50) NULL, 
+    [IsNDPRAccepted] BIT NOT NULL, 
+    [DateCreated] DATETIME NOT NULL, 
+    [CIF] NCHAR(10) NOT NULL, 
+    [CustomerName] NVARCHAR(MAX) NOT NULL, 
+    [FilePath] NVARCHAR(MAX) NOT NULL
+)
